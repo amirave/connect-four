b26a312bc6c47bf9e3048b7e6f7e5876cd10a2a1
amirave <amir.rave@gmail.com>
2023-03-01 18:37:36 +0200

    Removed unneeded imports

--- Assets/Scripts/AIPlayer.cs
+++ Assets/Scripts/AIPlayer.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Rendering;
-using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class AIPlayer : Player
